c0e87bd5ad695cc5dcf06c1d328a0a0f4ddc42ea
BrunoGomesCoelho <brasinhacoelho@hotmail.com>
2017-07-02 17:14:01 -0300

    funcao teste

---
+++ test.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <stdbool.h>
+
+
+bool is_EOF(FILE *stream) {
+    //verifica se é o final do arquivo de um stream.
+    char letra;
+    letra = fgetc(stream);
+    if(letra == EOF) {
+        ungetc(letra, stream);
+        return true;
+    }
+    ungetc(letra, stream);
+    return false;
+}
+
+
+int main (int argc, char *argv[]) {
+	FILE *fp;
+	fp = fopen("worst.idx", "rb");
+	int ticket, count = 0;
+	
+	while (!feof(fp)) {
+		fread(&ticket, sizeof(int), 1, fp);
+		printf("%d: %d \n", count, ticket);
+		count++;
+	}
+		
+	return 0;
+}
