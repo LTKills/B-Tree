0e6858e1bca60f837e377415c45c9732a5941dd0
Marcello <marcello_1000@yahoo.com.br>
2017-07-02 11:18:56 -0300

    Como a remove deve ser

--- src/remotion.c
+++ src/remotion.c
@@ -1,17 +1,42 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <string.h>
 #include <utils.h>
 #include <search.h>
-#include <check.h>
 
 void print_menu_remove() {
 	printf("Please, give the ticket number to be removed\n");
 	printf(">> ");
 }
 
+void logial_remove(FILE *fp, int pos, t_list *list) {
+	int counter = 0, invalid = -1;
+	char aux = 'a';
+
+	// Go to 'pos'
+	fseek(fp, pos, SEEK_SET);
+
+	// Go to the end of the record to count it's size
+	while (!feof(fp) && aux != '#') {
+		fread(&aux, sizeof(char), 1, fp);
+		if (!feof(fp)) counter++;
+	}
+
+	// Go back to pos
+	fseek(fp, pos, SEEK_SET);
+
+	// Mark the record as invalid
+	fwrite(&invalid, sizeof(int), 1, fp);	
+	
+	// Mark the record's size
+	fwrite(&counter, sizeof(int), 1, fp);
+
+	// Mark the next in the list
+	// fwrite top da lista
+
+}
+
 void remove_record(t_files *files, t_list *list) {
-	int ticket;
+	int ticket, byteOffset;
 
 	while (1) {
 		print_menu_remove();
@@ -20,4 +45,18 @@ void remove_record(t_files *files, t_list *list) {
 		printf("Ticket must be a integer greater then zero\n");
 	}
 
+	// Best fit
+	// byteOffset = busca_no_arquivo_de_indice_best
+	logial_remove(files->outputBest, byteOffset, list);
+	// list->best = byteOffset;
+
+	// Worst fit
+	// byteOffset = busca_no_arquivo_de_indice_worst
+	logial_remove(files->outputWorst, byteOffset, list);
+	// list->worst = byteOffset;
+
+	// First fit 
+	// byteOffset = busca_no_arquivo_de_indice_first
+	logial_remove(files->outputFirst, byteOffset, list);
+	// list->first = byteOffset;
 }
