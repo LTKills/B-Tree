93f093af19b414247b0730f75f5624188f14337a
Gabriel Cruz <gabs.oficial98@gmail.com>
2017-07-10 11:26:18 -0300

    cleaning college stuff

--- entrega/include/remotion.h
+++
@@ -1,8 +0,0 @@
-#ifndef _REMOTION_H_
-#define _REMOTION_H_
-
-#include <stdbool.h>
-
-bool remove_record(t_files *files, t_list *list);
-
-#endif
--- entrega/include/statistics.h
+++
@@ -1,9 +0,0 @@
-#ifndef _STATISTICS_H_
-#define _STATISTICS_H_
-
-
-void index_stats(t_files *files, t_list *lists);
-
-void removed_stats(t_files *files, t_list *lists);
-
-#endif
